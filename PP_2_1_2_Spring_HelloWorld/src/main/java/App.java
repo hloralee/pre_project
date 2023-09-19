c2461c7abbdba9661942e8c2ee2483095feacc4f
hloralee <hloralee@MacBook-Pro-de-hloralee.local>
2023-09-19 21:59:18 +0500

    added 2_1

---
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = (Cat) applicationContext.getBean(Cat.class);
+        System.out.println(cat1.getName());
+        System.out.println(cat2.getName());
+        System.out.println(cat1 == cat2);
+    }
+}
